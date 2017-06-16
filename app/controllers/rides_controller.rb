4a3396cf964c23a4961c21f8ae0ea1b87693c609
rkoko <rinda.ko@gmail.com>
2017-06-16 14:32:15 -0400

    spacing

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,6 +6,7 @@ class RidesController < ApplicationController
   def create
     @ride = Ride.new(user_id: current_user.id, attraction_id: params[:attraction_id])
     @message = @ride.take_ride
+    flash[:notice] = @message
     if @message = "Thanks for riding #{@ride.attraction.name}"
       @ride.save
       redirect_to user_path(@ride.user_id)
